fcb0e7486d2bc40d38eec6adc60ca46d8c536c4c
Paul Jaramillo <paulejaramillo@gmail.com>
2021-02-05 17:19:38 -0500

    Cleans up /flower request from App.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,17 +4,8 @@ import Axios from "axios";
 import Register from "./components/Register";
 
 function App() {
-  const [flower, setFlower] = useState({});
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  useEffect(async () => {
-    await Axios.get("/flower").then((result) => {
-      setFlower(result.data);
-    });
-  }, []);
   return (
     <div className="App">
-      {/* <h2>{flower.name}</h2>
-      <p>{flower.colour}</p> */}
       <Register />
     </div>
   );
