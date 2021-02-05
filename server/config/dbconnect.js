47372ee894fc2c7a07417fe5ae3416f555542d38
Paul Jaramillo <paulejaramillo@gmail.com>
2021-02-05 10:25:45 -0500

    setup dbconnect.js creds for local development

--- server/config/dbconnect.js
+++ server/config/dbconnect.js
@@ -1,22 +1,22 @@
 //Development Local Connection
 
-// module.exports = {
-//   dbconnect: {
-//     host: "localhost",
-//     user: "admin",
-//     password: "MateoPatricio@2015",
-//     database: "mysql-server-client-aws",
-//   },
-// };
+module.exports = {
+  dbconnect: {
+    host: "localhost",
+    user: "admin",
+    password: "MateoPatricio@2015",
+    database: "mysql-server-client-aws",
+  },
+};
 
 //AWS Connection
 
-module.exports = {
-  dbconnect: {
-    host: process.env.RDS_HOSTNAME,
-    user: process.env.RDS_USERNAME,
-    password: process.env.RDS_PASSWORD,
-    port: process.env.RDS_PORT,
-    database: "ebdb",
-  },
-};
+// module.exports = {
+//   dbconnect: {
+//     host: process.env.RDS_HOSTNAME,
+//     user: process.env.RDS_USERNAME,
+//     password: process.env.RDS_PASSWORD,
+//     port: process.env.RDS_PORT,
+//     database: "ebdb",
+//   },
+// };
