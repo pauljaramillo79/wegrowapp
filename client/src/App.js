dce960c62657664da6a1141cdf5a26ac3f5ce062
Paul Jaramillo <paulejaramillo@gmail.com>
2021-02-06 16:43:41 -0500

    Adds Context.Provider to App.js to track login state

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,48 @@
 import "./App.css";
-import React, { useState, useEffect } from "react";
-import Axios from "axios";
-import Register from "./components/Register";
+import React, { useContext, useReducer, createContext } from "react";
+import Login from "./components/Login";
+import Home from "./components/Home";
 
+export const AuthContext = createContext();
+
+const initialState = {
+  isAuthenticated: false,
+  user: null,
+  accesstoken: null,
+  refreshtoken: null,
+};
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "LOGIN":
+      localStorage.setItem("user", JSON.stringify(action.payload.user));
+      localStorage.setItem(
+        "refreshtoken",
+        JSON.stringify(action.payload.refreshtoken)
+      );
+      return {
+        ...state,
+        isAuthenticated: true,
+        user: action.payload.user,
+        refreshtoken: action.payload.refreshtoken,
+        accesstoken: action.payload.accesstoken,
+      };
+    case "LOGOUT":
+      localStorage.clear();
+      return {
+        ...state,
+        isAuthenticated: false,
+        user: null,
+      };
+    default:
+      return;
+  }
+};
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
   return (
-    <div className="App">
-      <Register />
-    </div>
+    <AuthContext.Provider value={{ state, dispatch }}>
+      <div className="App">{!state.isAuthenticated ? <Login /> : <Home />}</div>
+    </AuthContext.Provider>
   );
 }
 
