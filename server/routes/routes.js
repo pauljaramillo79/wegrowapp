8dbc94c5f58430460b30028947166bf19f1db885
Paul Jaramillo <paulejaramillo@gmail.com>
2021-02-19 15:52:41 -0500

    Small edits to routes.js

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -9,9 +9,14 @@ const jwt = require("jsonwebtoken");
 const authenticateToken = (req, res, next) => {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
-  if (token == null) return res.sendStatus(401);
+  if (token == null) {
+    res.status(401);
+    return res.send("Status 401: Unauthorized");
+  }
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    if (err) return res.sendStatus(403);
+    if (err) {
+      return res.sendStatus(403);
+    }
     req.user = user;
     next();
   });
