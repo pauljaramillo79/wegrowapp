80adeb046593c3e42bda374a363d0ae5538a9a12
Paul Jaramillo <paulejaramillo@gmail.com>
2021-02-22 11:58:21 -0500

    Prepare for deploy

--- server/config/dbconnect.js
+++ server/config/dbconnect.js
@@ -1,22 +1,22 @@
 //Development Local Connection
 
-module.exports = {
-  dbconnect: {
-    host: "localhost",
-    user: "admin",
-    password: "MateoPatricio@2015",
-    database: "weGrow",
-  },
-};
+// module.exports = {
+//   dbconnect: {
+//     host: "localhost",
+//     user: "admin",
+//     password: "MateoPatricio@2015",
+//     database: "weGrow",
+//   },
+// };
 
 //AWS Connection
 
-// module.exports = {
-//   dbconnect: {
-//     host: process.env.RDS_HOSTNAME,
-//     user: process.env.RDS_USERNAME,
-//     password: process.env.RDS_PASSWORD,
-//     port: process.env.RDS_PORT,
-//     database: "ebdb",
-//   },
-// };
+module.exports = {
+  dbconnect: {
+    host: process.env.RDS_HOSTNAME,
+    user: process.env.RDS_USERNAME,
+    password: process.env.RDS_PASSWORD,
+    port: process.env.RDS_PORT,
+    database: "ebdb",
+  },
+};
