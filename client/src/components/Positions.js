93662a93fa36d32d74f21f54a821b323fe7010dc
Paul Jaramillo <paulejaramillo@gmail.com>
2021-02-23 14:49:36 -0500

    fixing aws deploy

--- client/src/components/Positions.js
+++ client/src/components/Positions.js
@@ -4,9 +4,9 @@ import "./Positions.css";
 import Axios from "axios";
 
 const Positions = () => {
-  useEffect(() => {
-    Axios.post("/positions").then((result) => console.log(result.data));
-  }, []);
+  //   useEffect(() => {
+  //     Axios.post("/positions").then((result) => console.log(result.data));
+  //   }, []);
   // CONFIG is passed as a prop to TableSort component. Columns: in CONFIG must match the SQL query in /positions route in routes.js
   var CONFIG = {
     sort: { column: "year", order: "desc" },
@@ -33,7 +33,7 @@ const Positions = () => {
   return (
     <div className="positionslist">
       <h3 className="positionslisttitle">Positions List</h3>
-      {/* <PositionsTablesort config={CONFIG} /> */}
+      <PositionsTablesort config={CONFIG} />
     </div>
   );
 };
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -230,7 +230,7 @@ router.post("/positionreport", authenticateToken, async (req, res) => {
 });
 router.post("/positions", async (req, res) => {
   db.query(
-    "SELECT KTP AS WGP,positionID AS id, abbreviation, companyCode, packaging, shipmentStart AS Start, shipmentEnd AS End, concat('$',format(FOBCost,2)) AS FOB, format(quantityLow,2) AS quantity, year FROM positionsView",
+    "SELECT KTP AS WGP,positionID AS id, abbreviation, companyCode, packaging, shipmentStart AS Start, shipmentEnd AS End, concat('$',format(FOBCost,2)) AS FOB, format(quantityLow,2) AS quantity, year FROM positionsview",
     (err, results) => {
       if (err) {
         console.log(err);
