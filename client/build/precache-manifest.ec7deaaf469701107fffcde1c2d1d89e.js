7fe5f8a6b5a02624dc679406b77950434dc7541c
Paul Jaramillo <paulejaramillo@gmail.com>
2022-08-18 14:42:21 -0400

    run build

---
+++ client/build/precache-manifest.ec7deaaf469701107fffcde1c2d1d89e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce4e82604cf0ea1215a9",
+    "url": "/static/css/main.c67b6d6e.chunk.css"
+  },
+  {
+    "revision": "ce4e82604cf0ea1215a9",
+    "url": "/static/js/main.ce4e8260.chunk.js"
+  },
+  {
+    "revision": "fb3917f611c4520615c0",
+    "url": "/static/js/1.fb3917f6.chunk.js"
+  },
+  {
+    "revision": "4458c9567e3b0b5a7524",
+    "url": "/static/css/2.fce2fda6.chunk.css"
+  },
+  {
+    "revision": "4458c9567e3b0b5a7524",
+    "url": "/static/js/2.4458c956.chunk.js"
+  },
+  {
+    "revision": "67963a4431e61dae6b27",
+    "url": "/static/js/runtime~main.67963a44.js"
+  },
+  {
+    "revision": "a4f44d1ef0656721bcc0ce710e076d4d",
+    "url": "/static/media/logo-web.a4f44d1e.svg"
+  },
+  {
+    "revision": "5821caca5d369c1b27524bc16c50ded9",
+    "url": "/static/media/logo-simple.5821caca.svg"
+  },
+  {
+    "revision": "3ff9bd4999633376e15524ffa98daf8c",
+    "url": "/static/media/refreshicon.3ff9bd49.svg"
+  },
+  {
+    "revision": "7f914b0195e9b042215064dc86eabc61",
+    "url": "/static/media/unlocked.7f914b01.svg"
+  },
+  {
+    "revision": "d14db66376cc36168af83e2287761374",
+    "url": "/static/media/locked.d14db663.svg"
+  },
+  {
+    "revision": "7702178fa88a58ba80daca137dbeecf7",
+    "url": "/index.html"
+  }
+];
