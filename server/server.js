5f771eef3611113c9c0c4d5b73685817b33e0691
Paul Jaramillo <paulejaramillo@gmail.com>
2023-05-23 15:31:22 -0400

    check 2

--- server/server.js
+++ server/server.js
@@ -2,10 +2,6 @@ const express = require("express");
 const PORT = process.env.PORT || 4001;
 const path = require("path");
 const app = express();
-const http = require("http").createServer(app);
-
-var io = require("socket.io")(http);
-
 const bodyParser = require("body-parser");
 
 app.use(express.static(path.join(__dirname, "..", "client", "build")));
@@ -31,20 +27,3 @@ app.use("/", require("./routes/routes"));
 app.listen(PORT, () => {
   console.log(`Server listening in port ${PORT}`);
 });
-
-// http.listen(PORT, () => {
-//   let host = http.address().address;
-//   let port = http.address().port;
-//   console.log("App listening at http://%s:%s", host, port);
-// });
-
-// io.on("connection", (socket) => {
-//   console.log("Client connected to the websocket");
-//   socket.on("disconnect", () => {
-//     console.log("Client disconnected");
-//   });
-//   socket.on("sendmsg", (msg) => {
-//     console.log("Received a chat msg");
-//     io.emit("sendmsg");
-//   });
-// });
