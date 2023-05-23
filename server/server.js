b15db28b1ddc642be471656190a8d5228cdcc20e
Paul Jaramillo <paulejaramillo@gmail.com>
2023-05-23 15:27:39 -0400

    Testing socket.io express mixed server

--- server/server.js
+++ server/server.js
@@ -2,6 +2,10 @@ const express = require("express");
 const PORT = process.env.PORT || 4001;
 const path = require("path");
 const app = express();
+const http = require("http").createServer(app);
+
+var io = require("socket.io")(http);
+
 const bodyParser = require("body-parser");
 
 app.use(express.static(path.join(__dirname, "..", "client", "build")));
@@ -24,6 +28,23 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/", require("./routes/routes"));
 
-app.listen(PORT, () => {
-  console.log(`Server listening in port ${PORT}`);
+// app.listen(PORT, () => {
+//   console.log(`Server listening in port ${PORT}`);
+// });
+
+http.listen(PORT, () => {
+  let host = http.address().address;
+  let port = http.address().port;
+  console.log("App listening at http://%s:%s", host, port);
+});
+
+io.on("connection", (socket) => {
+  console.log("Client connected to the websocket");
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+  });
+  socket.on("sendmsg", (msg) => {
+    console.log("Received a chat msg");
+    io.emit("sendmsg");
+  });
 });
