0ed73bda2f61c46e56e38ec88135b55f804f81dc
Paul Jaramillo <paulejaramillo@gmail.com>
2023-06-13 11:40:56 -0400

    restricted access to AVB

--- client/src/components/AnalysisGrid.js
+++ client/src/components/AnalysisGrid.js
@@ -7,27 +7,37 @@ import Lyqsales from "./Lyqsales";
 import AVBGrid from "./AVBGrid";
 
 const AnalysisGrid = () => {
+  const role = JSON.parse(localStorage.getItem("role"));
+
   return (
     <div className="analysiscontainer">
       <div className="analysisnav">
         <NavLink activeClassName="navbaractive" to="/analysis/lyqsales" exact>
           Last Year Q Sales
         </NavLink>
-        <NavLink
-          activeClassName="navbaractive"
-          to="/analysis/actualvsbudget"
-          exact
-        >
-          Actual vs. Budget
-        </NavLink>
+        {role === 1 || role === 2 || role === 3 ? (
+          <NavLink
+            activeClassName="navbaractive"
+            to="/analysis/actualvsbudget"
+            exact
+          >
+            Actual vs. Budget
+          </NavLink>
+        ) : (
+          ""
+        )}
       </div>
       <div className="gridcontainer analysisgridcontainer">
         <Route path="/analysis/lyqsales">
           <Lyqsales />
         </Route>
-        <Route path="/analysis/actualvsbudget">
-          <AVBGrid />
-        </Route>
+        {role === 1 || role === 2 || role === 3 ? (
+          <Route path="/analysis/actualvsbudget">
+            <AVBGrid />
+          </Route>
+        ) : (
+          ""
+        )}
       </div>
     </div>
   );
