1d9bd9988522cf45637c1e4beb045eed9a57ad2f
Paul Jaramillo <paulejaramillo@gmail.com>
2024-08-22 09:38:47 -0400

    Add 2025 Budget page

--- client/src/components/BudgetGrid.jsx
+++ client/src/components/BudgetGrid.jsx
@@ -3,6 +3,7 @@ import { NavLink, Route } from "react-router-dom";
 import "./BudgetGrid.css";
 import Budget2023a from "./Budget2023a";
 import Budget2024a from "./Budget2024a";
+import Budget2025a from "./Budget2025a";
 
 const BudgetGrid = () => {
   const role = JSON.parse(localStorage.getItem("role"));
@@ -20,17 +21,25 @@ const BudgetGrid = () => {
         <NavLink activeClassName="navbaractive" to="/budget/budget2024" exact>
           2024
         </NavLink>
+        <NavLink activeClassName="navbaractive" to="/budget/budget2025" exact>
+          2025
+        </NavLink>
       </div>
       <div className="gridcontainer budgetgridcontainer">
         {role === 2 || role === 1 ? (
-          <Route path="/budget/budget2023">
-            <Budget2023a />
-          </Route>
+          <>
+            <Route path="/budget/budget2023">
+              <Budget2023a />
+            </Route>
+            <Route path="/budget/budget2024">
+              <Budget2024a />
+            </Route>
+          </>
         ) : (
           ""
         )}
-        <Route path="/budget/budget2024">
-          <Budget2024a />
+        <Route path="/budget/budget2025">
+          <Budget2025a />
         </Route>
       </div>
     </div>
